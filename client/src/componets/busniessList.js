0dfea177a68af0e11645a7b71f34c8e0dce87258
Sam Roy-Effer <samroyeffer@gmail.com>
2021-08-20 11:39:30 -0400

    reinstall back end

---
+++ client/src/componets/busniessList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Router } from "react-router-dom";
+
+export default function FrontPage() {
+  return (
+    <Router>
+      <section>
+        <div classname="bizId">Where id will go</div>
+        <div classname="bizName">Where buiness name will go</div>
+        <div className="websiteURL">WHere website url will fog</div>
+        <div className="Address"></div>
+      </section>
+    </Router>
+  );
+}
